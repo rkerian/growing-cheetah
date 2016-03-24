97f7d51986759e3a95748c48b45871958edccae8
Ryan Kerian <rkerian@gmail.com>
2016-03-24 23:02:49 +0000

    Added user error messaging

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -32,6 +36,11 @@ class PlacesController < ApplicationController
       return render text: 'Not Allowed', status: :forbidden
     end
     @place.update_attributes(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
     redirect_to root_path
   end
 

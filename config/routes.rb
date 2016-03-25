c3545817941049adbcb2645905fd11b1c53f591e
Ryan Kerian <rkerian@gmail.com>
2016-03-25 15:26:50 +0000

    Built out commenting system and DB integration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Nomster::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 root 'places#index'
-resources :places
+resources :places do
+  resources :comments, only: :create
+end
 devise_for :users
 
   # You can have the root of your site routed with "root"
